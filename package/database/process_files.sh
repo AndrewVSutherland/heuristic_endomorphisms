38ee699d4038035d6f66c8f0c85ab1300fd64e11
AndrewVSutherland <drew@math.mit.edu>
2016-06-23 15:41:09 -0400

    process_files.sh

--- package/database/process_files.sh
+++ package/database/process_files.sh
@@ -3,6 +3,6 @@ FILES=input*
 for f in $FILES
 do
     id="${f[@]:5:2}"
-    echo "load(\"process_file.sage\"); process_file(\"input${id}\",\"output${id}\",fh_index=3,st_index=6,prec=500)" >process_${f}.sage
+    echo "load (\"../Initialize.sage\"); load(\"process_file.sage\"); process_file(\"input${id}\",\"output${id}\",fh_index=3,st_index=6,prec=500)" >process_${f}.sage
     /math/software/pkg/sage/64/current/sage process_${f}.sage &
 done
