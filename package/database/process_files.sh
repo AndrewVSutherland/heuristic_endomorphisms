07c94ef1624105051505224a231818b2a15d499a
AndrewVSutherland <drew@math.mit.edu>
2016-06-23 21:37:53 -0400

    add killloop.sh

--- package/database/process_files.sh
+++ package/database/process_files.sh
@@ -3,5 +3,5 @@ FILES=input*
 for f in $FILES
 do
     id="${f[@]:5:2}"
-    /math/software/pkg/sage/64/current/sage -c "__endodir__=\"/math/home/drew/github/heuristic_endomorphisms/package/\"; load(__endodir__+\"Initialize.sage\"); load(__endodir__+\"database/process_file.sage\"); process_file(\"input${id}\",\"output${id}\",fh_index=3,st_index=6,prec=500, hell=[0])" &
+    /math/software/pkg/sage/64/current/sage -c "__endodir__=\"/math/home/drew/github/heuristic_endomorphisms/package/\"; load(__endodir__+\"Initialize.sage\"); load(__endodir__+\"database/process_file.sage\"); process_file(\"input${id}\",\"output${id}\",fh_index=3,st_index=6,prec=500,B=4,maxrun=100)" &
 done
