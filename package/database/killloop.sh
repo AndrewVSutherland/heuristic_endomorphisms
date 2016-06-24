07a3ce03979332f9428f5c81d5b31383eacffa22
AndrewVSutherland <drew@math.mit.edu>
2016-06-23 21:38:59 -0400

    add killloop.sh

--- package/database/killloop.sh
+++ package/database/killloop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [ 1 ]
-    ps -aux | grep "drew" | grep "magma.intel64" | awk '{if ($5+0 > 10) print $2}' | xargs kill
+    ps -aux | grep "drew" | grep "magma.intel64" | awk '{if ($5+0 > 5) print $2}' | xargs kill
     sleep 30s
 done
